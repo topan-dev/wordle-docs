f35a034ca4d16dced947d9b9fa1ffc7dd8b427d2
Milmon <115549630@qq.com>
2023-02-21 08:55:35 +0800

    home page init

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,57 @@
-import { defineUserConfig } from 'vuepress'
+import { defineUserConfig, defaultTheme, } from 'vuepress'
+import { shikiPlugin } from '@vuepress/plugin-shiki'
+import { searchPlugin } from '@vuepress/plugin-search'
+import { gitPlugin } from '@vuepress/plugin-git'
 
 export default defineUserConfig({
-  lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
-  base: '/wordle-docs/'
-})
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'Wordle',
+      description: '',
+    },
+    '/zh/': {
+      lang: 'zh-CN',
+      title: 'Wordle',
+      description: '',
+    },
+  },
+  head: [['link', { rel: 'shortcut icon', type: 'image/x-icon', href: 'https://molmin.github.io/f/system/favicon.ico' }]],
+  base: '/wordle-docs/',
+  theme: defaultTheme({
+    locales: {
+      '/': {
+        selectLanguageName: 'English',
+      },
+      '/zh/': {
+        selectLanguageName: '简体中文',
+      },
+    },
+  }),
+  plugins: [
+    shikiPlugin({}),
+    searchPlugin({
+      locales: {
+        '/': {
+          placeholder: 'Search',
+        },
+        '/zh/': {
+          placeholder: '搜索',
+        },
+      },
+      maxSuggestions: 10
+    }),
+    gitPlugin({
+      locales: {
+        '/': {
+          lastUpdatedText: 'Last Updated',
+          contributorsText: 'Contributorsqwq',
+        },
+        '/zh/': {
+          lastUpdatedText: '最后编辑于',
+          contributorsText: '贡献者'
+        },
+      }
+    })
+  ]
+});
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,17 @@
-# Hello VuePress
+## Wordle
+
+Here is a multiparty interactive game on web - Wordle.
+
+You need to guess a word, and we will give each letter a color, and the colors must be red, yellow and green. Grey means that this letter isn't in the answer word. Yellow means that this letter appears in the answer word but not on its right position. Green means that you guess the letter on its right position.
+
+For example, the answer is 'apple', and you give the word 'pears'. Because 'p', 'e', 'a' is in the answer and not in the correct position, so it'll show orange-orange-orange-grey-grey. If you give the word 'paper', it'll be orange-orange-green-orange-grey. If you give the word 'paple', it'll be orange-orange-green-green-green.
+
+As mentioned above is Standard Rule. In addition, we also provide some more difficult and interesting rules for you to use.
+
+### Contributors
+
+[Details](https://github.com/topan-dev/wordle/graphs/contributors)
+
+### Contact us
+
+[Email - milmon@qq.com](mailto:milmon@qq.com)
